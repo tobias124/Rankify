b72237328e2eaee4e0184ddc189b3245164e18f0
abdoelsayed2016 <abdoelsayed2016@gmail.com>
2025-02-10 00:24:49 +0100

    edit version

--- rankify/__version__.py
+++ rankify/__version__.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev29+gd173f91.d20241125'
-__version_tuple__ = version_tuple = (0, 1, 'dev29', 'gd173f91.d20241125')
+__version__ = version = '0.1.0.post1'
+__version_tuple__ = version_tuple = (0, 1, '0', 'post1')
