7ff18f3291e9272538a37f9c92f2b89d02b46ac1
abdoelsayed2016 <abdoelsayed2016@gmail.com>
2025-02-10 04:24:29 +0100

    edit pip install to include cpp file for colbert

--- examples/retreiver.py
+++ examples/retreiver.py
@@ -20,10 +20,41 @@ documents = [
     Document(question=Question("Who wrote Hamlet?"), answers=Answer(["Shakespeare"]), contexts=[])
 ]
 
-retriever = Retriever(method="bge", n_docs=5 , index_type="wiki" )
+
+
+
+retriever = Retriever(method="colbert", n_docs=1 , index_type="wiki" )
 retrieved_documents = retriever.retrieve(documents)
 
 # Print the first retrieved document
 for i, doc in enumerate(retrieved_documents):
     print(f"\nDocument {i+1}:")
     print(doc)
+
+retriever = Retriever(method="colbert", n_docs=1 , index_type="msmarco" )
+retrieved_documents = retriever.retrieve(documents)
+
+
+# Print the first retrieved document
+for i, doc in enumerate(retrieved_documents):
+    print(f"\nDocument {i+1}:")
+    print(doc)
+
+
+retriever = Retriever(method="bge", n_docs=1 , index_type="wiki" )
+retrieved_documents = retriever.retrieve(documents)
+
+# Print the first retrieved document
+for i, doc in enumerate(retrieved_documents):
+    print(f"\nDocument {i+1}:")
+    print(doc)
+
+retriever = Retriever(method="bge", n_docs=1 , index_type="msmarco" )
+retrieved_documents = retriever.retrieve(documents)
+# Print the first retrieved document
+for i, doc in enumerate(retrieved_documents):
+    print(f"\nDocument {i+1}:")
+    print(doc)
+
+
+
