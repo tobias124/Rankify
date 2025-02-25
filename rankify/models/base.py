cf80c081f8b8a8dfbe0768af326c1068bbf0ef55
abdoelsayed2016 <abdoelsayed2016@gmail.com>
2025-02-25 01:09:01 +0100

    add docstring to base

--- rankify/models/base.py
+++ rankify/models/base.py
@@ -7,28 +7,28 @@ class BaseRanking(ABC):
 
     This class defines the interface for all ranking models, ensuring that all subclasses implement the required methods.
 
-    Attributes
-    ----------
-    method : str
-        The name of the ranking method.
-    model_name : str
-        The name of the model being used for ranking.
-    api_key : str, optional
-        An optional API key for accessing remote models or services.
+    Attributes:
+        method (str): The name of the ranking method.
+        model_name (str): The name of the model being used for ranking.
+        api_key (str, optional): An optional API key for accessing remote models or services.
     """
+
     @abstractmethod
     def __init__(self, method: str= None, model_name: str= None, api_key: str= None, **kwargs) ->None:
         """
         Initializes the base ranking model.
 
-        Parameters
-        ----------
-        method : str, optional
-            The name of the ranking method (default is None).
-        model_name : str, optional
-            The name of the model being used for ranking (default is None).
-        api_key : str, optional
-            An optional API key for accessing remote models or services (default is None).
+        Args:
+            method (str, optional): The name of the ranking method. Defaults to None.
+            model_name (str, optional): The name of the model being used for ranking. Defaults to None.
+            api_key (str, optional): An optional API key for accessing remote models or services. Defaults to None.
+
+        Example:
+            ```python
+            class MyRanking(BaseRanking):
+                def __init__(self, method, model_name):
+                    super().__init__(method, model_name)
+            ```
         """
         pass
 
@@ -37,21 +37,22 @@ class BaseRanking(ABC):
         """
         Abstract method to rank a list of documents.
 
-        Parameters
-        ----------
-        document : list of Document
-            A list of Document instances that need to be ranked.
+        Args:
+            documents (list[Document]): A list of Document instances that need to be ranked.
 
-        Examples
-        --------
-        This method should be implemented by subclasses to provide a ranking mechanism.
-        
-        >>> class MyRanking(BaseRanking):
-        >>>     def __init__(self, method, model_name):
-        >>>         super().__init__(method, model_name)
-        >>>     def rank(self, document):
-        >>>         # Ranking implementation here
-        >>>         pass
+        Raises:
+            NotImplementedError: This method must be implemented by subclasses.
+
+        Example:
+            ```python
+            class MyRanking(BaseRanking):
+                def __init__(self, method, model_name):
+                    super().__init__(method, model_name)
+
+                def rank(self, documents):
+                    # Ranking implementation here
+                    pass
+            ```
         """
         pass
 
