90f3194240a290e1d1d9f009a99476ebddf59192
aherzinger <user@DISC-gaming.>
2025-05-15 15:16:01 +0200

    basic ralm works with new implementation, config loading missing

---
+++ examples/generator_ralm.py
@@ -0,0 +1,21 @@
+from rankify.dataset.dataset import Document, Question, Answer, Context
+from rankify.generator.generator import Generator
+
+# Define question and answer
+question = Question("What is the capital of France?")
+#answers = Answer(["Paris"])
+answers=Answer("")
+contexts = [
+    Context(id=1, title="France", text="The capital of France is Paris.", score=0.9),
+    Context(id=2, title="Germany", text="Berlin is the capital of Germany.", score=0.5)
+]
+
+# Construct document
+doc = Document(question=question, answers=answers, contexts=contexts)
+
+# Initialize Generator (e.g., Meta Llama)
+generator = Generator(method="in-context-ralm", model_name='meta-llama/Meta-Llama-3.1-8B-Instruct', backend="huggingface")
+
+# Generate answer
+generated_answers = generator.generator.answer_question([doc])
+print(generated_answers)  # Output: ["Paris"]
--- rankify/generator/generator.py
+++ rankify/generator/generator.py
@@ -4,10 +4,11 @@ from rankify.generator.rag_methods.chain_of_thought_rag import ChainOfThoughtRAG
 from rankify.generator.models.huggingface_model import HuggingFaceModel
 from rankify.generator.fid import FiDGenerator
 from rankify.generator.in_context_ralm import InContextRALMGenerator
+from rankify.generator.rag_methods.in_context_ralm_rag import InContextRALMRAG
 
 GENERATOR_MODELS = {
     #"fid": FiDGenerator,
-    #"in-context-ralm": InContextRALMGenerator,
+    "in-context-ralm": InContextRALMRAG,
     #"huggingface": HuggingFaceModel,  # Add this line
     "basic-rag": BasicRAG,
     "chain-of-thought-rag": ChainOfThoughtRAG,
@@ -73,6 +74,8 @@ class Generator:
             self.generator = BasicRAG(model)
         elif method == "chain-of-thought-rag":
             self.generator = ChainOfThoughtRAG(model)
+        elif method == "in-context-ralm":
+            self.generator = InContextRALMRAG(model)
         else:
             # For other methods, directly assign the model
             self.generator = model
