505df8881867eb4f9d7e3816ce4da140f5e67ff1
aherzinger <user@DISC-gaming.>
2025-05-16 16:56:09 +0200

    refactored init of generator

---
+++ rankify/utils/generator/generator_models.py
@@ -0,0 +1,12 @@
+from rankify.generator.rag_methods.basic_rag import BasicRAG
+from rankify.generator.rag_methods.chain_of_thought_rag import ChainOfThoughtRAG
+from rankify.generator.rag_methods.fid_rag_method import FiDRAGMethod
+from rankify.generator.rag_methods.in_context_ralm_rag import InContextRALMRAG
+
+
+GENERATOR_MODELS = {
+    "in-context-ralm": InContextRALMRAG,
+    "fid": FiDRAGMethod,
+    "basic-rag": BasicRAG,
+    "chain-of-thought-rag": ChainOfThoughtRAG,
+}
