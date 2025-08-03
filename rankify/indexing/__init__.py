716bb095ac98071505c9243f005e1df6ec77f8d4
abdoelsayed2016 <abdoelsayed2016@gmail.com>
2025-08-03 09:55:34 +0200

    edit

--- rankify/indexing/__init__.py
+++ rankify/indexing/__init__.py
@@ -2,5 +2,6 @@ from .lucene_indexer import LuceneIndexer
 from .dpr_indexer import DPRIndexer
 from .contriever_indexer import ContrieverIndexer
 from .colbert_indexer import ColBERTIndexer
+from .bge_indexer import BGEIndexer
 
-__all__ = ["LuceneIndexer", "DPRIndexer", "ContrieverIndexer", "ColBERTIndexer"]
+__all__ = ["LuceneIndexer", "DPRIndexer", "ContrieverIndexer", "ColBERTIndexer", "BGEIndexer"]
